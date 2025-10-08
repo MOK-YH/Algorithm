b2c2d13a3de3de75201731ec2428975facbcff31
목영후 <dudgn3015@naver.com>
2025-10-08 16:11:59 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -2,6 +2,5 @@ def solution(n):
     answer = 0
     
     for i in range(1, 6 * n +1):
-        for j in range(1, 10):
-            if (6 * i) % n == 0:
+        if (6 * i) % n == 0:
                 return i
