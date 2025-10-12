51a88e940176f6f5d90e229f00d470971153523d
목영후 <dudgn3015@naver.com>
2025-10-12 13:48:22 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,9 +1,9 @@
 def solution(angle):
     if 0 < angle < 90:
-        return 1   # 예각
+        return 1
     elif angle == 90:
-        return 2   # 직각
+        return 2
     elif 90 < angle < 180:
-        return 3   # 둔각
+        return 3
     elif angle == 180:
-        return 4   # 평각
+        return 4
