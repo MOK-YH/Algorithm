301c4a1b38ba8f278a755a7579c11dd61f523fa3
목영후 <dudgn3015@naver.com>
2025-10-22 15:32:47 +0900

    [level 0] Title: 합성수 찾기, Time: 0.49 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,10 +1,10 @@
 def solution(n):
-    cnt = 0
-    for i in range(1, n + 1):
-        divisor = 0
-        for j in range(1, i + 1):
+    answer = 0
+    for i in range(1, n+1):
+        cnt = 0
+        for j in range(1, i+1):
             if i % j == 0:
-                divisor += 1
-        if divisor >= 3:  # 약수가 3개 이상이면 합성수
-            cnt += 1
-    return cnt
+                cnt += 1
+        if cnt >= 3:
+            answer += 1        
+    return answer
